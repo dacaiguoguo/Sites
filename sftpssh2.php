f39cbb9d5fc714b579f9d08c459fc0b0d8252233
dacaiguoguo <dacaiguoguo@163.com>
2015-03-03 21:52:24 +0800

    iOS应用打包 & upload

--- sftpssh2.php
+++ sftpssh2.php
@@ -1,82 +1,65 @@
+<html>
+	<head>
+		<title>iOS应用打包</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	</head>
+	<body style="text-align:center">
+	<?php
+	echo '<br />';	
+	echo  "kaishi target....ooooo" ;
+	echo '<br />';	
+	
+	?>
+
 <?php
 
-echo  "kaishi target...." ;
-$cmd = "./buildipa.sh";
-$url = system($cmd);
-if($url != 1){
-	echo '<br />';
-	echo "not return 1";
-	echo '<br />';	
-} else {
-	echo " return 1";
-	echo '<br />';	
-}
-
-$connection = ssh2_connect('192.168.0.96', 22);
-if(!$connection){
-	echo 'errot';
-} else {
-	echo 'success';
-}
-echo '<br />';
-if(!ssh2_auth_password($connection, 'root', 'admin508956')){
-	echo "can not ssh2_auth_password";
-}
-
-$sftp = ssh2_sftp($connection);
-if(!$sftp){
-	echo 'errot sftp';
-} else {
-	echo 'success 22sftp';
-	echo '<br />';
-	$sendbol = ssh2_scp_send($connection, "/Users/dacaiguo/Sites/myzipfile.zip", "/opt/web/client/debugapps/myzipfile.zip", 0777);  //上传文件
-	if($sendbol ){
-		ECHO  'yeyyyyy';
+	echo  "kaishi target...." ;
+	$cmd = "./buildipa.sh";
+	$url = system($cmd);
+	if($url != 1){
+		echo '<br />';
+		echo "not return 1";
+		echo '<br />';	
 	} else {
-		ECHO  'noooooo';
+		echo " return 1";
+		echo '<br />';	
 	}
-	
-	$recvFileBool = ssh2_scp_recv($connection, "/opt/web/client/debugapps/myzipfile.zip", "/Users/dacaiguo/Sites/myzipfilerevvvv.zip");
-	// bool ssh2_scp_recv ( resource $session , string $remote_file , string $local_file )
-	
-	if($recvFileBool ){
-		ECHO  '$recvFileBool yeyyyyy';
+
+	$connection = ssh2_connect('192.168.0.96', 22);
+	if(!$connection){
+		echo 'errot';
 	} else {
-		ECHO  '$recvFileBool noooooo';
+		echo 'success';
 	}
+	echo '<br />';
+	if(!ssh2_auth_password($connection, 'root', 'admin508956')){
+		echo "can not ssh2_auth_password";
+	}
+
+	$sftp = ssh2_sftp($connection);
+	if(!$sftp){
+		echo 'errot sftp';
+	} else {
+		echo 'success 22sftp';
+		echo '<br />';
+		$sendbol = ssh2_scp_send($connection, "/Users/dacaiguo/Documents/workplace/lvmama630/Lvmm.ipa", "/opt/web/client/debugapps/iphone/640/debug/Lvmm.ipa");  //上传文件
+		if($sendbol ){
+			ECHO  'yeyyyyy';
+		} else {
+			ECHO  'noooooo';
+		}
 	
-}
-
-
-
-
-$path = '123.html';
-$date = date('Y-m-d_H.i.s');
-$path .= '/'.$date;
-$filename="/Users/dacaiguo/Sites/myzipfile.zip"; //文件名
-ssh2_sftp_mchkdir($sftp,$path);
-$path.='/new'.$filename;
-
-
-
-
-$stream = fopen("ssh2.sftp://$sftp/opt/web/client/debugapps/dev.html", 'r');
-echo '<br />';
-if(!$stream){
-	echo 'errot stream';
-} else {
-	echo 'success stream';
-	echo file_get_contents("ssh2.sftp://$sftp/123.html");
-}
-// $fileContent = ssh2_scp_recv($connection, "ssh2.sftp://$sftp/opt/web/client/debugapps/dev.html", './filename');;
-$fileContent = ssh2_scp_recv($connection, "ssh2.sftp://$sftp/123.html", './filename');;
-
-echo '<br />';
-echo ssh2_sftp_realpath($sftp,".");
-echo '<br />';
-if(!$fileContent){
-	echo 'errot $fileContent';
-} else {
-	echo 'success $fileContent';
-}
+		$recvFileBool = ssh2_scp_recv($connection, "/opt/web/client/debugapps/myzipfile.zip", "/Users/dacaiguo/Sites/myzipfilerevvvv.zip");
+		// bool ssh2_scp_recv ( resource $session , string $remote_file , string $local_file )
+	
+		if($recvFileBool ){
+			ECHO  '$recvFileBool yeyyyyy';
+		} else {
+			ECHO  '$recvFileBool noooooo';
+		}
+	
+	}
 ?>
+	</body>
+
+</html>
